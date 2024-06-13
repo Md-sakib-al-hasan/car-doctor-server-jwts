353c9a34a91483011762939b4fce2c7424d4e36f
Md-sakib-al-hasan <mdsakibalhasanprogrammer1@gmail.com>
2024-06-13 09:50:01 +0600

    endz_project

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+var cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin:['http://localhost:5173'],
+    credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+const logger = async(req,res,next) => {
+    console.log('clled:',req.host , req.originalUrl)
+    next(); 
+}
+
+const VrifyToken = async(req,res,next) => {
+    const token = req.cookies?.token;
+    console.log("value of token in middleware",token)
+    if(!token){
+        return res.status(401).send({message:"forbinent"})
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,docoded) => {
+        if(err){
+            return res.status(401).send({message:"unauthenized"})
+        }
+        req.user = docoded
+        next()
+    })
+
+    
+}
 
 
 console.log(process.env.DB_PASS)
@@ -27,11 +55,21 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-
+        //auth related api
+         app.post('/jwt',logger, async (req,res) => {
+            const user =req.body;
+            const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'})
+            res.cookie('token',token,{
+                httpOnly:true,
+                secure:false
+            })
+            .send({success: true})
+         })
+        //services related api
         const serviceCollection = client.db('Cars-doctors').collection('services');
         const bookingCollection = client.db('Cars-doctors').collection('booking');
 
-        app.get('/services', async (req, res) => {
+        app.get('/services',logger, async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -52,11 +90,16 @@ async function run() {
 
 
         // bookings 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings',logger,VrifyToken, async (req, res) => {
             console.log(req.query.email);
+            // console.log('tok tok token',req.cookies.token)
+            console.log('user in the valid token',req.user)
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
+            }
+            if(req.query.email !== req.user.email){
+                return res.status(403).send({message:"forbibent"})
             }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
